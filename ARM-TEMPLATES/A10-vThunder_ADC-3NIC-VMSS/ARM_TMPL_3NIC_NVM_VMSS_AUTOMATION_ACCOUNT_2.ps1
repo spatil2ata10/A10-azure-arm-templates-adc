b04aae9976b842ca437c8f42fb16a347ec821860
spatil2ata10 <119580880+spatil2ata10@users.noreply.github.com>
2023-02-28 15:32:52 +0530

    Update ARM_TMPL_3NIC_NVM_VMSS_AUTOMATION_ACCOUNT_2.ps1

--- ARM-TEMPLATES/A10-vThunder_ADC-3NIC-VMSS/ARM_TMPL_3NIC_NVM_VMSS_AUTOMATION_ACCOUNT_2.ps1
+++ ARM-TEMPLATES/A10-vThunder_ADC-3NIC-VMSS/ARM_TMPL_3NIC_NVM_VMSS_AUTOMATION_ACCOUNT_2.ps1
@@ -19,12 +19,13 @@ if ($null -eq $paramData) {
 $azureAutoScaleResources = $paramData.azureAutoScaleResources  | ConvertTo-Json
 $glmParam = $paramData.glmParam  | ConvertTo-Json
 $sslParam = $paramData.sslParam  | ConvertTo-Json
-$slbParam = $paramData.slbParam  | ConvertTo-Json -Depth 5
+$slbParam = $paramData.slbParam  | ConvertTo-Json -Depth 4
 $autoScaleParam = $paramData.autoScaleParam | ConvertTo-Json
 $vThunderIP = $paramData.vThunderIP
 $clientSecret = $paramData.clientSecret
 $resourceGroupName = $paramData.azureAutoScaleResources.resourceGroupName
 $automationAccountName = $paramData.azureAutoScaleResources.automationAccountName
+$location = $paramData.azureAutoScaleResources.location
 $vCPUUsage = $paramData.vCPUUsage
 $agentPrivateIP = $paramData.agentPrivateIP
 $vThUsername = $paramData.vThUserName
@@ -38,23 +39,11 @@ $vThCurrentPasswordVal = $vThNewPasswordVal
 $vThCurrentPassword = ConvertFrom-SecureString -SecureString $vThCurrentPasswordVal -AsPlainText
 $vThNewPassword = ConvertFrom-SecureString -SecureString $vThNewPasswordVal -AsPlainText
 $vThPasswordc = Read-Host "Confirm New Password" -AsSecureString
-$vThPasswordConfirm = ConvertFrom-SecureString -SecureString $vThPasswordc -AsPlainText
+$vThPasswordConfirm = ConvertFrom-SecureString -SecureString $vThPasswordc -AsPlainText 
 
 if ($vThNewPassword -ne $vThPasswordConfirm) {
     Write-Error "New Password doesn't match." -ErrorAction Stop
 }
-
-$logAnalyticsWorkspaceName = $paramData.azureAutoScaleResources.logAnalyticsWorkspaceName
-$azureLogMetrics = $paramData.azureLogMetricsParam  | ConvertTo-Json
-
-$vmss_obj = Get-AzVmss -ResourceGroupName $resourceGroupName -VMScaleSetName $paramData.azureAutoScaleResources.vThunderScaleSetName
-$vmssResourceId = $vmss_obj.Id
-
-$workspace_obj = Get-AzOperationalInsightsWorkspace -ResourceGroupName $resourceGroupName -Name $logAnalyticsWorkspaceName
-$workspaceId = $workspace_obj.CustomerId
-
-$shared_key_obj = Get-AzOperationalInsightsWorkspaceSharedKey -ResourceGroupName $resourceGroupName -Name $logAnalyticsWorkspaceName
-$sharedKey = $shared_key_obj.PrimarySharedKey
 #Create runbook variables
 New-AzAutomationVariable -AutomationAccountName $automationAccountName -Name "azureAutoScaleResources" -Encrypted $False -Value $azureAutoScaleResources -ResourceGroupName $resourceGroupName
 
@@ -83,11 +72,3 @@ New-AzAutomationVariable -AutomationAccountName $automationAccountName -Name "vT
 New-AzAutomationVariable -AutomationAccountName $automationAccountName -Name "vThNewPassword" -Encrypted $True -Value $vThNewPassword -ResourceGroupName $resourceGroupName
 
 New-AzAutomationVariable -AutomationAccountName $automationAccountName -Name "vThNewPassApplyFlag" -Encrypted $False -Value $isPasswordChangesForAll -ResourceGroupName $resourceGroupName
-
-New-AzAutomationVariable -AutomationAccountName $automationAccountName -Name "workspaceId" -Encrypted $False -Value $workspaceId -ResourceGroupName $resourceGroupName
-
-New-AzAutomationVariable -AutomationAccountName $automationAccountName -Name "sharedKey" -Encrypted $True -Value $sharedKey -ResourceGroupName $resourceGroupName
-
-New-AzAutomationVariable -AutomationAccountName $automationAccountName -Name "vmssResourceId" -Encrypted $False -Value $vmssResourceId -ResourceGroupName $resourceGroupName
-
-New-AzAutomationVariable -AutomationAccountName $automationAccountName -Name "azureLogMetricsParam" -Encrypted $False -Value $azureLogMetrics -ResourceGroupName $resourceGroupName
